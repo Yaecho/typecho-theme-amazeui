f8e0ef849e2fa7c192c3201334cde68ed3fcf3f4
TTjxc <dz1401@aliyun.com>
2016-03-18 17:27:33 +0800

    video

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 <style type="text/css">
-	.ttjxc-post img{
+	.ttjxc-post img,.ttjxc-post video{
 	  display: block;
       max-width: 100%;
       height: auto;
